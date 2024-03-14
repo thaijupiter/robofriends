509d6db126e0b2d037d1b85cef6cf57e5ae1d179
Thailik <thailik@Thais-MacBook-Pro-2.local>
2024-03-13 20:41:49 -0400

    updated w/ hooks

---
+++ src/robots.js
@@ -0,0 +1,68 @@
+export const robots = [
+{  
+	id: 1,
+	name: 'Smino',
+	username: 'SmiBot',
+	email: 'smi@zero.com'
+},
+{
+	id: 2,
+	name: 'Thundercat',
+	username: 'ashydaddy',
+	email: 'turbotron2000@cat.com'
+},
+
+{
+	id: 3,
+	name: 'Ravyn',
+	username: 'moonshoes',
+	email: 'hypnos@3d.com'
+},	
+
+{
+	id: 4,
+	name: 'Meg',
+	username: 'hotgirlkujo',
+	email: 'theestallion@ahhh.net'
+},
+{
+	id: 5,
+	name: 'Gorillaz',
+	username: 'dirtyharry',
+	email: 'gorillaz@plasticbeach.net'
+},
+{
+	id: 6, 
+	name: 'Spike',
+	username: 'spacecowboy',
+	email: 'seeyou@bebop.com'
+},
+
+{
+	id: 7,
+	name: 'Naruto',
+	username: 'futurehokage',
+	email: 'believeit@konoha.com'
+},
+
+{
+	id: 8,
+	name: 'Chicken Joe',
+	username: 'joethechicken',
+	email: 'surfchicken@wave.net'
+},
+
+{
+	id: 9,
+	name: 'Katara',
+	username: 'watertribegirl',
+	email: 'hopeandpeace@harmony.com'
+},
+
+{
+	id: 10,
+	name: 'Tobi Lou',
+	username: 'hitobi',
+	email: 'tobiandtheloop@farada.com'
+}
+	]
